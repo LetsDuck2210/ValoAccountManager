27c65a1c407d01310ac3fc181246f0c77e12ccde
st182932 <st182932@stud.uni-stuttgart.de>
2023-06-13 09:30:41 +0200

    AddAccountPanel now extends AccountPanel

--- src/gui/panels/AddAccountPanel.java
+++ src/gui/panels/AddAccountPanel.java
@@ -1,66 +1,25 @@
 package gui.panels;
 
-import java.awt.GridLayout;
 import java.util.ArrayList;
-import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.MatteBorder;
-
-import gui.GuiConstants;
-import gui.components.CurrencySelect;
-import gui.components.DoubleInputField;
-import gui.components.Headline;
 import gui.components.Input;
-import gui.components.InputField;
 import main.ValoAccountManager;
 import valorant.Account;
 import valorant.Currency;
 
-public class AddAccountPanel extends JPanel {
+public class AddAccountPanel extends AccountPanel {
 	private static final long serialVersionUID = -1081282648038615739L;
-	private List<Input> inputs = new ArrayList<>();
-
+	
 	public AddAccountPanel() {
-		var layout = new GridLayout(8, 1);
-		setLayout(layout);
-		setBackground(GuiConstants.BACKGROUND_COLOR);
-		setOpaque(true);
-
-		add(new Headline("Add Account"));
-		inputs.add(new InputField("ID: "));
-		inputs.add(new InputField("Password: "));
-		inputs.add(new DoubleInputField("Name: ", "# "));
-		inputs.add(new CurrencySelect());
-		inputs.add(new InputField("Notes: "));
-
-		for (var input : inputs)
-			add((JComponent) input);
-
-		var submit = new JButton();
-		submit.setText("submit");
-		submit.setOpaque(true);
-		submit.setBackground(GuiConstants.COMPONENT_COLOR);
-		submit.setForeground(GuiConstants.TEXT_COLOR);
-		submit.setFont(GuiConstants.FONT);
-		submit.addActionListener(a -> {
-			if (informationFilled()) {
-				ValoAccountManager.addAccount(fromInput());
-				clearInputs();
-
-			}
-		});
-		submit.setBorder(BorderFactory.createCompoundBorder(new MatteBorder(5, 0, 10, 5, GuiConstants.BACKGROUND_COLOR),
-				new EmptyBorder(5, 0, 10, 5)));
-
-		add(new JComponent() {
-			private static final long serialVersionUID = 1045449690138475005L;
-		});
-		add(submit);
+		super("Add Account");
+	}
+	
+	@Override
+	void submit() {
+		if (informationFilled()) {
+			ValoAccountManager.addAccount(fromInput());
+			clearInputs();
+		}
 	}
 
 	private Account fromInput() {
@@ -79,12 +38,12 @@ public class AddAccountPanel extends JPanel {
 		return new Account(riotId, password, name, tag, additional, currency);
 	}
 
-	private void clearInputs() {
+	void clearInputs() {
 		for (var input : inputs)
 			input.clear();
 	}
 
-	private boolean informationFilled() {
+	boolean informationFilled() {
 		var filled = true;
 		var neccessaryInputs = new ArrayList<Input>();
 		for (int i = 0; i < 3; i++)
@@ -103,4 +62,5 @@ public class AddAccountPanel extends JPanel {
 
 		return filled;
 	}
+
 }
